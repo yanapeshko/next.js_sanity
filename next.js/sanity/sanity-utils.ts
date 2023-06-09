6dc934b315db7eaf7b93c67891ec61319c9104c8
Yana Peshko <yanapeshko917@gmail.com>
2023-06-08 21:59:49 -0400

    added Project-type to utils

--- next.js/sanity/sanity-utils.ts
+++ next.js/sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
+import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 
-export async function getProjects() {
+export async function getProjects(): Promise<Project[]> {
   const client = createClient({
     projectId: "5q9psij4",
     dataset: "production",
