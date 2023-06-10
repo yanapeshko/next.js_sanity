57e15c4d7f01431e4f0d6df8069160a1b906e853
Yana Peshko <yanapeshko917@gmail.com>
2023-06-09 23:41:59 -0400

    started adding styles

--- next.js/app/page.tsx
+++ next.js/app/page.tsx
@@ -1,13 +1,43 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <div className="max-w-5xl mx-auto py-20">
+      <h1 className="text-7xl font-extrabold">
+        Hello I&apos;m{" "}
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+          Yana
+        </span>
+        !
+      </h1>
+      <p className="mt-3 text-xl text-gray-600">
+        Hola everyone! Check out my projects!
+      </p>
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {projects.map((project) => (
+          <div
+            key={project._id}
+            className="border border-gray-500 rounded-lg p-3"
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={100}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+              {project.name}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
