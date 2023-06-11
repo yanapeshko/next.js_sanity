0edc5e824387781a3c86f9b7c1ced67bcb83de9b
Yana Peshko <yanapeshko917@gmail.com>
2023-06-11 18:20:47 -0400

    styling

--- next.js/app/projects/[project]/page.tsx
+++ next.js/app/projects/[project]/page.tsx
@@ -7,5 +7,19 @@ type Props = {
 export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
-  return <div>{project.name}</div>;
+  return (
+    <div>
+      <header>
+        <h1>{project.name}</h1>
+        <a
+          href={project.url}
+          title="View Poject"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Project
+        </a>
+      </header>
+    </div>
+  );
 }
