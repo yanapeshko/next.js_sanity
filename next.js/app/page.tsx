7cf90a6d1596b5f1abb075673ae72c5fbbdbfb61
Yana Peshko <yanapeshko917@gmail.com>
2023-06-12 17:08:27 -0400

    styling+organizing

--- next.js/app/page.tsx
+++ next.js/app/page.tsx
@@ -6,9 +6,9 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="max-w-5xl mx-auto py-20">
+    <div>
       <h1 className="text-7xl font-extrabold">
-        Hello I&apos;m
+        Hello I&apos;m {``}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
           Yana
         </span>
